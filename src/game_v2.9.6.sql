66a0b0103a5845e14f1b061385aac07742c7c70c
xiexinlin <1172531013@qq.comzz>
2022-07-11 15:29:40 +0800

    2.9.6版本更新： 奖励删除符文等级废弃字段

--- src/game_v2.9.6.sql
+++ src/game_v2.9.6.sql
@@ -12,3 +12,26 @@ CREATE TABLE `obtain_log`  (
   KEY (`user_id`) USING BTREE,
   KEY (`ref_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4;
+
+-- 奖励删除符文等级废弃字段
+alter table mail_reward drop column rune_level;
+alter table task_reward drop column rune_level;
+alter table lottery_reward drop column rune_level;
+alter table common_reward drop column rune_level;
+
+
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (1, '物理攻击', 1, 5, '增加百分比物理攻击', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (2, '物理防御', 2, 5, '增加百分比物理防御', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (3, '魔法攻击', 3, 5, '增加百分比魔法攻击', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (4, '魔法抗性', 4, 5, '增加百分比魔法抗性', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (5, '速度', 5, 5, '增加百分比速度', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (6, '生命值', 6, 8, '增加百分比生命值', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (7, '物理暴击', 7, 5, '增加百分比物理暴击', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (8, '物理暴伤', 8, 8, '增加百分比物理暴伤', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (9, '魔法暴击', 9, 5, '增加百分比魔法暴击', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (10, '魔法暴伤', 10, 8, '增加百分比魔法暴伤', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (11, '物理吸血', 11, 5, '增加百分比物理吸血', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (12, '魔法吸血', 12, 5, '增加百分比魔法吸血', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (13, '物理穿透', 13, 5, '增加百分比物理穿透', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (14, '魔法穿透', 14, 5, '增加百分比魔法穿透', '2021-05-26 19:30:16', b'1');
+INSERT INTO `game_test`.`rune`(`id`, `name`, `type`, `value`, `remark`, `create_time`, `status`) VALUES (15, '治疗效果', 15, 5, '增加百分比治疗效果', '2021-05-26 19:30:16', b'1');
