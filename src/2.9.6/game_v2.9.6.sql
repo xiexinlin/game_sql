e4368551d1a07962279b38fb11c8e64627036182
xxl <1172531013@qq.com>
2022-07-17 23:12:30 +0800

    2.9.6版本更新： sql脚本补充

--- src/2.9.6/game_v2.9.6.sql
+++ src/2.9.6/game_v2.9.6.sql
@@ -11,3 +11,16 @@ CREATE TABLE `obtain_log`  (
   KEY (`user_id`) USING BTREE,
   KEY (`ref_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4;
+
+
+-- 2.9.6 版本
+INSERT INTO `version`(`id`, `version`, `token`, `is_valid`, `create_time`, `send_flag`) VALUES (7, '2.9.6', '73BFC6FBB6B0FC1DD646DCE617794C96', 1, now(), 0);
+
+-- 2.9.6 版本更新补偿奖励
+INSERT INTO common_reward(`id`, `ref_type`, `ref_id`, `type`, `reward_id`, `num`, `random`) VALUES (4, 'VERSION_UPGRADE', 7, 2, NULL, 10000, NULL);
+
+
+INSERT INTO `announcement`(`id`, `version`, `title`, `release_time`, `create_time`, `type`, `content`) VALUES (14, '2.9.5', '2022-07-17 2.9.6版本更新公告', '2022-07-17', now(), 'VERSION_UPGRADE', NULL);
+
+INSERT INTO `announcement_detail`(`announcement_id`, `content`, `type`) VALUES (14, '本地资源动态加载优化', '1');
+INSERT INTO `announcement_detail`(`announcement_id`, `content`, `type`) VALUES (14, '首页未读消息红点提示（邮件，任务完成，好友消息，新英雄，新物品提示）', '1');
