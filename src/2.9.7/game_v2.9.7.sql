e23ca6bdcd8ae8ba49d5f4b001fb29d40644d9f6
xiexinlin <1172531013@qq.comzz>
2022-07-27 17:33:56 +0800

    2.9.7版本更新： 新增道具招募券

--- src/2.9.7/game_v2.9.7.sql
+++ src/2.9.7/game_v2.9.7.sql
@@ -2,3 +2,6 @@ alter table user_rune add column is_valid tinyint(1) comment '是否有效';
 alter table user_rune add column update_time datetime comment '更新时间';
 
 update user_rune set is_valid = 1 where is_valid is null;
+
+INSERT INTO `article`(`id`, `name`, `type`, `hero_id`, `description`) VALUES (998, '招募券', 2, NULL, '可招募一次英雄');
+INSERT INTO `shop_article`(`id`, `article_id`, `currency`, `cost`, `sort`, `sale_date_start`, `sale_date_end`, `is_date_limited`, `daily_max_limit`) VALUES (10, 998, 2, 100, 10, NULL, NULL, 0, NULL);
