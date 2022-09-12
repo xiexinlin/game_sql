5b8f988047f2209b81c41e54b1f798079a933d2b
admin <1172531013@qq.com>
2022-09-12 16:55:47 +0800

    2.10.2版本sql整理

--- src/2.10.2/game_v2.10.2.sql
+++ src/2.10.2/game_v2.10.2.sql
@@ -11,3 +11,14 @@ CREATE TABLE `hero_train_log`  (
 	KEY (`user_id`) USING BTREE,
 	KEY (`hero_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4;
+
+-- 2.10.2 版本
+INSERT INTO `version`(`id`, `version`, `token`, `is_valid`, `create_time`, `send_flag`) VALUES (12, '2.10.2', '3611C4A71F667AE1DFF83A37FD8636B7', 1, now(), 0);
+insert into reward(source, source_id, type, reward_id, num) values ('VERSION_UPGRADE', 12, 'DIAMOND', NULL, 10000);
+
+
+INSERT INTO `announcement`(`id`, `version`, `title`, `release_time`, `create_time`, `type`, `content`) VALUES (19, '2.10.2', '2022-09-12 2.10.2版本更新公告', '2022-09-12', now(), 'VERSION_UPGRADE', NULL);
+
+INSERT INTO `announcement_detail`(`announcement_id`, `content`, `type`) VALUES (19, '英雄羁绊系统完善', '1');
+INSERT INTO `announcement_detail`(`announcement_id`, `content`, `type`) VALUES (19, '英雄助战系统', '1');
+INSERT INTO `announcement_detail`(`announcement_id`, `content`, `type`) VALUES (19, '阵容界面调整', '1');
