17542a769b3dc0370d4904c13ba5bc54207570a9
xxl <1172531013@qq.com>
2022-09-18 23:25:23 +0800

    2.10.4版本更新： sql整理

---
+++ src/hero_relation.sql
@@ -0,0 +1,3 @@
+INSERT INTO `hero_relation`(`id`, `hero_id`, `other_id`, `name`, `attribute`, `create_time`) VALUES (1, 1, 2, '剑之归途', '+10%物理攻击', now());
+INSERT INTO `hero_relation`(`id`, `hero_id`, `other_id`, `name`, `attribute`, `create_time`) VALUES (2, 2, 1, '剑之归途', '+10%物理攻击', now());
+
