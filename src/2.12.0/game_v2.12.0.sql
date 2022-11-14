afd6cb67d4543434ed014b0aef744f58ecfb6fea
xxl <1172531013@qq.com>
2022-11-14 23:19:25 +0800

    2.12.0版本更新： sql整理

---
+++ src/2.12.0/game_v2.12.0.sql
@@ -0,0 +1,18 @@
+alter table designation change combatPowerAddition combat_power_addition int(0) not null comment '战斗力加成';
+
+update designation set combat_power_addition = 100 where id = 1;
+update designation set combat_power_addition = 150 where id = 2;
+update designation set combat_power_addition = 240 where id = 3;
+update designation set combat_power_addition = 360 where id = 4;
+update designation set combat_power_addition = 500 where id = 5;
+update designation set combat_power_addition = 80 where id = 11;
+update designation set combat_power_addition = 120 where id = 12;
+update designation set combat_power_addition = 450 where id = 100;
+update designation set combat_power_addition = 500 where id = 500;
+update designation set combat_power_addition = 450 where id = 800;
+update designation set combat_power_addition = 150 where id = 900;
+update designation set combat_power_addition = 210 where id = 901;
+update designation set combat_power_addition = 250 where id = 902;
+update designation set combat_power_addition = 350 where id = 903;
+update designation set combat_power_addition = 550 where id = 904;
+update designation set combat_power_addition = 600 where id = 1000;
