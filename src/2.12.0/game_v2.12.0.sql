c45b5db0a668a807cd718d112b9e65e823b544a7
xxl <1172531013@qq.com>
2022-11-17 22:14:22 +0800

    2.12.0版本更新： 阵容属性加成

--- src/2.12.0/game_v2.12.0.sql
+++ src/2.12.0/game_v2.12.0.sql
@@ -16,3 +16,15 @@ update designation set combat_power_addition = 250 where id = 902;
 update designation set combat_power_addition = 350 where id = 903;
 update designation set combat_power_addition = 550 where id = 904;
 update designation set combat_power_addition = 600 where id = 1000;
+
+-- 阵容属性加成
+create table battle_array_detail_attribute (
+	 `id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+	 `battle_array_detail_id` int(0) NOT NULL COMMENT '阵容明细id',
+	 `type` varchar(20) not null comment '属性类型',
+	 `addition` int(0) not null comment '加成',
+	 `is_valid` int(0) not null comment '是否有效',
+	 `create_time` datetime COMMENT '创建时间',
+	 PRIMARY KEY (`id`) USING BTREE,
+	 KEY (`battle_array_detail_id`) USING BTREE
+)
