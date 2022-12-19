cf6d5bb083208f058e0ed9a6eb871c1884caae3c
xxl <1172531013@qq.com>
2022-12-20 00:40:37 +0800

    2.13.0版本更新： 英雄招募

---
+++ src/2.13.0/game_v2.13.0.sql
@@ -0,0 +1,56 @@
+create table hero_recruitment (
+	`id` int(0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+	`hero_id` int(0) NOT NULL COMMENT '英雄id',
+	`type` varchar(20) NOT NULL COMMENT '类型',
+	`is_valid` int(0) NOT NULL COMMENT '是否有效',
+	`create_time` datetime COMMENT '创建时间',
+	`update_time` datetime COMMENT '更新时间',
+	PRIMARY KEY (`id`) USING BTREE,
+	KEY (`hero_id`) USING BTREE
+)
+
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (1, 1, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (2, 2, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (3, 3, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (4, 4, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (5, 5, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (6, 6, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (7, 7, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (8, 8, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (9, 9, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (10, 10, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (11, 11, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (12, 12, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (13, 13, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (14, 14, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (15, 15, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (16, 16, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (17, 17, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (18, 18, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (19, 19, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (20, 20, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (21, 21, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (22, 22, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (23, 23, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (24, 24, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (25, 25, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (26, 26, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (27, 27, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (28, 28, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (29, 29, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (30, 30, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (31, 31, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (32, 32, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (33, 33, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (34, 34, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (35, 35, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (36, 36, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (37, 37, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (38, 38, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (39, 39, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (40, 40, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (41, 41, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (42, 42, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (43, 43, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+INSERT INTO `game_test`.`hero_recruitment`(`id`, `hero_id`, `type`, `is_valid`, `create_time`, `update_time`) VALUES (44, 44, 'COMMON', 1, '2022-12-19 20:27:08', NULL);
+
